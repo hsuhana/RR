dc4841ca8788ef8861dcc6f6061052a6a38b4329
tracy <200548417@student.georgianc.on.ca>
2024-11-15 11:02:24 -0500

    add mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,52 @@
+var configs = require("./configs/globals");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var session = require("express-session");
+var passport = require("passport");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//mongodb
+var mongoose = require("mongoose");
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var membersRouter = require('./routes/members');
 
 var cors = require('cors');
 
+var Member = require('./models/member');
+
 var app = express();
-
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "secretSession",
+  resave: false,
+  saveUninitialized: false
+}));
 
+//mongodb and login setting
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(Member.createStrategy());
+// Set passport to write/read user data to/from session object
+passport.serializeUser(Member.serializeUser());
+passport.deserializeUser(Member.deserializeUser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/members', membersRouter);
 
 app.use(cors());
+
+//connect to mongodb
+mongoose.connect(configs.ConnectionStrings.MongoDB)
+.then(() => {console.log("Connected to MongoDB!");})
+.catch((error) => {console.error("Error connecting to MongoDB:", error);});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
