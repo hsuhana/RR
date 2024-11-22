abbaef029828ff7794a4186ead77db5cdee8ac3a
tracy <200548417@student.georgianc.on.ca>
2024-11-22 09:16:18 -0500

    add reservation file in routes folder in backend

--- backend/routes/reservations.js
+++ backend/routes/reservations.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Reservation = require('../models/reservation');
+var Table = require('../models/table');
+var passport = require('passport');
+const { isAuthenticated } = require('../utils/auth');
 
 
+
+
+// POST /reservations
+// router.post('/reservation', isAuthenticated, async (req, res) => {
+//     try{
+//         const { tableId, reservationDate, reservationTime, numberOfGuest, specialRequests } = req.body;
+
+
+
+//     }
+// }); 
 
 module.exports = router;
