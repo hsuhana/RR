c36e4e1255f8d216facfe17e55ad623afa273f04
tracy <200548417@student.georgianc.on.ca>
2024-11-28 18:07:54 -0500

    add menu image to menu page and change the font

--- front/src/pages/Menu.js
+++ front/src/pages/Menu.js
@@ -1,10 +1,17 @@
 import React from "react";
+import ramen_menu1 from "../images/ramen_menu1.png";
+import ramen_menu2 from "../images/ramen_menu2.png";
 
 const Menu = () => {
     return (
-        <div>
-            <h1>Menu Page</h1>
-        </div>
+        <>
+            <div className="menuBackgorund">
+                <h1>MENU</h1>
+                <img className="ramen_menu1" src={ramen_menu1} alt="amen_menu1" />
+                <img className="ramen_menu2" src={ramen_menu2} alt="amen_menu2" />
+            </div>
+        </>
+
     );
 };
 
