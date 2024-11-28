d4e0880c2c7a718994ee795cd623c84a40a4f816
Kaori Kumura <kaori.k239@gmail.com>
2024-11-28 11:18:43 -0500

    Made some changes feedback pages

---
+++ front/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <h1>Ramen Restrant</h1>
+      </div>
+
+    </header>
+  );
+}
+
+export default Header;
+
