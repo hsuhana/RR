ed5fede16c29a359d8288f6787bc6239927a0207
tracy <200548417@student.georgianc.on.ca>
2024-12-09 23:58:40 -0500

    change apiService setting for cors pass

--- front/src/apiService.js
+++ front/src/apiService.js
@@ -2,7 +2,10 @@
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const fetchData = () => {
-  return fetch(`${apiUrl}/apiService`)
+  return fetch('https://rr-y88w.vercel.app/apiService', {
+    method: 'GET', // or 'POST', depending on the request
+    mode: 'no-cors', // Set mode to 'no-cors'
+  })
     .then((response) => response.json())
     .then((data) => data);
 };
