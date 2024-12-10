49ed3eda99525ee556b16e2392742af9547e6168
tracy <200548417@student.georgianc.on.ca>
2024-12-10 00:37:57 -0500

    change cors setting

--- front/src/apiService.js
+++ front/src/apiService.js
@@ -4,7 +4,7 @@ const apiUrl = process.env.REACT_APP_API_URL;
 export const fetchData = () => {
   return fetch(`${apiUrl}/api`, {  // Change from `/api/index.js` to `/api`
     method: 'GET',
-    mode: 'cors',  // Use 'cors' instead of 'no-cors'
+    mode: 'no-cors',  // Use 'cors' instead of 'no-cors'
   })
   .then((response) => response.json())
   .then((data) => data);
