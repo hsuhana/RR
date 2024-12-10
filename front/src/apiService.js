596346e31eeb1735741c3a2531e74ce67abd6864
tracy <200548417@student.georgianc.on.ca>
2024-12-10 00:45:09 -0500

    cors a

--- front/src/apiService.js
+++ front/src/apiService.js
@@ -2,7 +2,10 @@
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const fetchData = () => {
-  return fetch(`${apiUrl}/api`) // Change from `/api/index.js` to `/api`
+  return fetch(`${apiUrl}/api`, {  // Make sure the URL is correct
+    method: 'GET', // or 'POST', depending on the request
+    mode: 'cors', // Use 'cors' to enable proper cross-origin requests
+  })
   .then((response) => response.json())
   .then((data) => data);
 };
