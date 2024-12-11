bf1a763f0eeddf5a5d1943f3ef1d358a32065507
tracy <200548417@student.georgianc.on.ca>
2024-12-11 17:17:01 -0500

    a

--- backend/api/index.js
+++ backend/api/index.js
@@ -70,8 +70,6 @@ app.use((err, req, res, next) => {
   res.send('error');
 });
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
 
 // Server setup
 const port = normalizePort(process.env.PORT || "5001");
