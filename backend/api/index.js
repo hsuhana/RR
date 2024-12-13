8696d869703a42d46be13cc2f595873525ffc179
tracy <200548417@student.georgianc.on.ca>
2024-12-13 18:19:19 -0500

    20

--- backend/api/index.js
+++ backend/api/index.js
@@ -41,13 +41,13 @@ app.use(cors({
 }));
 
 // Handle preflight requests manually if needed
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://rr-juvb.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(200);  // Respond with 200 OK for preflight requests
-});
+// app.options('*', (req, res) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://rr-juvb.vercel.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.sendStatus(200);  // Respond with 200 OK for preflight requests
+// });
 
 
 
