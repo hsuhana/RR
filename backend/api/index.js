6f03d2f19991b271960a12ad0268cddd085e8046
tracy <200548417@student.georgianc.on.ca>
2024-12-13 17:34:22 -0500

    14

--- backend/api/index.js
+++ backend/api/index.js
@@ -74,6 +74,7 @@ app.use(session({
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 1 day
+    sameSite: 'None',
    }, // Use true for HTTPS connections
 }));
 
